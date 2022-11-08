1de4714a7857a5de66484d65e4cb303a87a0b9bf
GabrielNBDS <gabriel.nbds@gmail.com>
2022-11-08 13:10:31 -0400

    👌 IMPROVE: Clear, add hotwire turbo and remove lucid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
 import '../css/app.css'
 import './alpine'
-import './turbolinks'
+import './turbo'
--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,3 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('home')
-})
-
-Route.get('/about', async ({ view }) => {
-  return view.render('about')
-})
+Route.on('/').render('pages/home')
