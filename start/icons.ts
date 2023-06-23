8bb0d7dcb2b26fe95f087f5aa3ed3148cd5e02e4
GabrielNBDS <gabriel.nbds@gmail.com>
2023-06-23 16:47:15 -0400

    📦 NEW: change icons to lucide and fix sidebar

--- start/icons.ts
+++ start/icons.ts
@@ -1,6 +1,6 @@
 import View from '@ioc:Adonis/Core/View'
 import { edgeIconify, addCollection } from 'edge-iconify'
-import { icons as ph } from '@iconify-json/ph'
+import { icons as lucide } from '@iconify-json/lucide'
 
 View.use(edgeIconify)
-addCollection(ph)
+addCollection(lucide)
